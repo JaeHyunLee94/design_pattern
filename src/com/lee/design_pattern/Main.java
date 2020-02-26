a502acbee9088330b90b3fd81a8f4f59466ea800
LEE JAE HYUN <leejaehyun1223@gmail.com>
2020-02-27 00:43:39 +0900

    singleton

--- src/com/lee/design_pattern/Main.java
+++ src/com/lee/design_pattern/Main.java
@@ -1,12 +1,18 @@
 package com.lee.design_pattern;
 
+import com.lee.design_pattern.Singleton.Singleton;
+
 public class Main {
 
     public static void main(String[] args) {
 
 
+        Singleton ins= Singleton.getSingleton_instance();
+
+        Singleton ins2= Singleton.getSingleton_instance();
 
 
+        System.out.println(ins.equals(ins2));
 
 
     }
--- src/com/lee/design_pattern/Singleton/Singleton.java
+++ src/com/lee/design_pattern/Singleton/Singleton.java
@@ -1,4 +1,15 @@
 package com.lee.design_pattern.Singleton;
 
 public class Singleton {
+
+
+    private static Singleton singleton_instance;
+
+
+    public static Singleton getSingleton_instance() {
+        return singleton_instance==null? singleton_instance=new Singleton() : singleton_instance;
+    }
+
+
+
 }
